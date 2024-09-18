2e3fd12bd729d6d123266a42e55308ba9a1576d0
Dominik <dominikbielecki.kontakt@gmail.com>
2024-09-18 14:32:06 +0200

    few sections done

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,12 +3,133 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title> 
+  <title>Transport</title> 
   @vite(['resources/js/app.js', 'public/js/app.js', 'public/scss/style.scss'])
 </head>
 <body>
+  <div class="socials__header">
+    <div class="socials__header-container">
+      <div class="links">
+        <a href="#">FAQ</a>
+        <span>|</span>
+        <a href="#">Helpdesk</a>
+        <span>|</span>
+        <a href="#">Track Shipment</a>
+      </div>
+      <div class="socials">
+        <div>twt</div>
+        <div>fb</div>
+      </div>
+    </div>
+  </div>
+  <header class="logo__header">
+    <div class="logo__header-container">
+      <div class="logo__container">
+        <img src="{{ asset('/img/logo.png') }}" alt="Logo">
+      </div>
+      <div class="points__container">
+        <div class="point">
+          <div class="point__text">
+            <h4>WE ARE HERE</h4>
+            <p>West Yorkshire - UK</p>
+          </div>
+          <div class="point__img">
+            <img src="{{ asset('/img/location_icon.png') }}" alt="Location Icon">
+          </div>
+        </div>
+        <div class="point">
+          <div class="point__text">
+            <h4>CALL US ON</h4>
+            <p>
+              <a href="tel:+12123456789">+12 123 456 789</a>
+            </p>
+          </div>
+          <div class="point__img">
+            <img src="{{ asset('/img/phone_icon.png') }}" alt="Phone Icon">
+          </div>
+        </div>
+        <div class="point clock__point">
+          <div class="point__text">
+            <h4>WE ARE OPEN</h4>
+            <p>9.00 AM - 6.00 PM</p>
+          </div>
+          <div class="point__img">
+            <img src="{{ asset('/img/clock_icon.png') }}" alt="Clock Icon">
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <nav class="navbar navbar-expand-md">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">SERVICES</a></li>
+          <li><a href="#">OUR SPECIALITIES</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <main>
     {{ $slot }}
   </main>
+  <footer>
+    <div class="footer__container">
+      <div class="footer__element footer__logo">
+        <div class="image__container">
+          <img src="{{ asset('/img/logo_white.png') }}" alt="Logo">
+        </div>
+        <p>
+          At [Your Company Name], we specialize in providing reliable and efficient transport and removal services. With a commitment to quality and sustainability, our experienced team ensures safe delivery of furniture, eco-friendly disposal of old items, and seamless moving solutions. Trust us to handle your needs with care and professionalism.
+        </p>
+      </div>
+      <div class="footer__element">
+        <h2>Navigation</h2>
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">SERVICES</a></li>
+          <li><a href="#">OUR SPECIALITIES</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+      </div>
+      <div class="footer__element">
+        <h2>Section</h2>
+        <ul>
+          <li><a href="#">THIS</a></li>
+          <li><a href="#">IS</a></li>
+          <li><a href="#">SOME</a></li>
+          <li><a href="#">LIST</a></li>
+        </ul>
+      </div>
+      <div class="footer__element footer__contact">
+        <h2>Contact us</h2>
+        <div>
+          <p>50 Main Design Street</p>
+          <p>West Yorkshire - UK</p>
+        </div>
+        <div>
+          <p>
+            <a href="tel:">+1 (234) 5678 90,</a>
+          </p>
+          <p>
+            <a href="tel:">+1 (234) 5678 90</a>
+          </p>
+        </div>
+        <div>
+          <p>
+            <a href="mailto:">office@transport.com</a>
+          </p>
+          <p>
+            <a href="mailto:">info@transport.com</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,136 @@
 <x-layout>
-    <div class="mt-5">something</div>
+    <div>
+        <section class="hero">HERO SECTION SPACE</section>
+        <section class="whyus__section">
+            <div class="section__container">
+                <div class="text__container">
+                    <h2>OUR SERVICES</h2>
+                    <div class="line">
+                        <div class="blue"></div>
+                        <div class="gray"></div>
+                    </div>
+                    <p>Expert Care and Reliable Service for All Your Moving Needs</p>
+                </div>
+                <div class="cards__container">
+                    <div class="card">
+                        <div class="image__container">
+                            <img src="{{ asset('img/truck_icon.png') }}" alt="Truck Icon">
+                        </div>
+                        <h2>Reliable Sofa Transport</h2>
+                        <p>Safe and careful delivery of your sofa and large furniture. We ensure your items arrive in perfect condition.</p>
+                    </div>
+                    <div class="card">
+                        <div class="image__container">
+                            <img src="{{ asset('img/eco_icon.png') }}" alt="Truck Icon">
+                        </div>
+                        <h2>Eco-Friendly Furniture Removal</h2>
+                        <p>Hassle-free pickup and responsible disposal of old sofas, chairs, and beds. We handle your waste sustainably.</p>
+                    </div>
+                    <div class="card">
+                        <div class="image__container">
+                            <img class="icon-filter" src="{{ asset('img/pallet_icon.png') }}" alt="Truck Icon">
+                        </div>
+                        <h2>Swift Moving & Pallet Transport</h2>
+                        <p>Efficient moving services and secure pallet transport for a smooth transition. Reliable and timely solutions.</p>
+                    </div>
+            </div>
+        </section>
+        <section class="bestat__section">
+            <div class="section__container">
+                <div class="text__container">
+                    <h2>OUR SPECIALITIES</h2>
+                    <div class="line">
+                        <div class="blue"></div>
+                        <div class="gray"></div>
+                    </div>
+                    <p>Leading the Way in Quality and Reliability</p>
+                </div>
+                <div class="grid__container">
+                    <div class="attributes">
+                        <div class="attribute">
+                            <div class="text__container">
+                                <h3>Fast Response and Flexible Scheduling</h3>
+                                <p>We provide quick response times and adaptable scheduling to fit your busy life, ensuring a smooth and convenient service experience.</p>
+                            </div>
+                            <div class="image__container">
+                                <img src="{{ asset('img/calendar_icon.png') }}" alt="Calendar icon">
+                            </div>
+                        </div>
+                        <div class="attribute">
+                            <div class="text__container">
+                                <h3>Precision and Care in Furniture Transport</h3>
+                                <p>Our team excels in handling and transporting your furniture with meticulous care, ensuring every piece arrives in perfect condition.</p>
+                            </div>
+                            <div class="image__container">
+                                <img src="{{ asset('img/sofa_icon.png') }}" alt="Sofa icon">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="main__image__container">
+                        image placeholder
+                    </div>
+                    
+                    <div class="attributes">
+                        <div class="attribute">
+                            <div class="image__container">
+                                <img src="{{ asset('img/money_icon.png') }}" alt="Money icon">
+                            </div>
+                            <div class="text__container">
+                                <h3>Affordable Pricing without Compromise on Quality</h3>
+                                <p>Our competitive pricing ensures you get top-notch service without breaking the bank, offering great value for your investment.</p>
+                            </div>
+                        </div>
+                        <div class="attribute">
+                            <div class="image__container">
+                                <img src="{{ asset('img/truck2_icon.png') }}" alt="Truck icon">
+                            </div>
+                            <div class="text__container">
+                                <h3>Efficient and Reliable Moving Services</h3>
+                                <p>We specialize in seamless and timely moving solutions, whether for residential relocations or commercial pallet transport, providing reliable service every time.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="contact__section">
+            <div class="contact__section-container">
+                <div class="text__container">
+                    <h2>CONTACT US</h2>
+                    <div class="line">
+                        <div class="blue"></div>
+                        <div class="gray"></div>
+                    </div>
+                    <p>Contact Us for Prompt and Friendly Support</p>
+                </div>
+                <form action="">
+                    <div class="form__container">
+                        <div class="input__container">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" required placeholder="Enter your name">
+                        </div>
+                        <div class="input__container">
+                            <label for="lastname">Last name</label>
+                            <input type="text" id="lastname" required placeholder="Enter your lastname">
+                        </div>
+                        <div class="input__container">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" required placeholder="Enter your email">
+                        </div>
+                        <div class="input__container">
+                            <label for="phone">Phone number</label>
+                            <input type="number" id="phone" required placeholder="Enter your phone number">
+                        </div>
+                        <div class="textarea__container">
+                            <textarea name="message" rows="5" id="message" required placeholder="Enter your message"></textarea>
+                        </div>
+                        <div class="btn__container">
+                            <button type="submit">Send</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </div>
 </x-layout>
